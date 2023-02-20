ccaa81be37bf6d37593da45449fb0a574cc8bfc4
Antonio Gabriel González Casado <casado84@gmail.com>
2023-02-20 23:05:40 +0100

    Commit inicial

---
+++ src/main/java/org/iesalixar/daw2/dawspringauthenticatingldap/DawSpringAuthenticatingLdapApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.dawspringauthenticatingldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawSpringAuthenticatingLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawSpringAuthenticatingLdapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesalixar/daw2/dawspringauthenticatingldap/DawSpringAuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.dawspringauthenticatingldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawSpringAuthenticatingLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
